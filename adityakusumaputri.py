c96f060eb5329bf791468514c41d6208b42ba7b5
Adityakusumaputri <113223571+Adityakusumaputri@users.noreply.github.com>
2022-09-16 13:36:02 +0700

    Aditya Kusumaputri tugas pelatihan coding

---
+++ adityakusumaputri.py
@@ -0,0 +1,16 @@
+print ("Menghitung Volume Balok")
+p = float(input("masukan nilai p : "))
+l = float(input("masukan nilai l : "))
+t = float(input("masukan nilai t : "))
+
+volume = p*l*t
+
+print("volume balok adalah :", (p*l*t))
+
+print("Menghitung Volume Kubus")
+
+s = float(input("masukan panjang sisi : "))
+
+volume = s*s*s
+
+print ("volume kubus adalah :", (s*s*s))
